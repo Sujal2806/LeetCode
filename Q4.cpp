3cf23ecbc65ad4ca9e068ea4147d31a8e8ce98b6
Sujal2806 <sujalg2806@gmail.com>
2024-01-19 19:37:10 +0530

    Median of Two Sorted Arrays

--- Q4.cpp
+++ Q4.cpp
@@ -1,6 +1,9 @@
 //4) Median of Two Sorted Arrays
 
 /*Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.*/
+
+#include<bits/stdc++.h>
+using namespace std;
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
