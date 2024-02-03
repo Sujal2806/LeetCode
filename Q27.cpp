cb24d4640afdf9fd605265f81cf2f3884469726a
Sujal Gupta <111141823+Sujal2806@users.noreply.github.com>
2024-02-04 05:13:33 +0530

    Remove Element

---
+++ Q27.cpp
@@ -0,0 +1,19 @@
+//27) Remove Element
+/*Given an integer array nums and an integer val, remove all occurrences of val in nums in-place.
+The order of the elements may be changed. 
+Then return the number of elements in nums which are not equal to val.*/
+
+using namespace std;
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+};
