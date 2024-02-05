c2a9224380dbd0bcdafb7554cdf2205f75b8bad9
Sujal Gupta <111141823+Sujal2806@users.noreply.github.com>
2024-02-05 19:39:42 +0530

    Next Permutation

---
+++ Q31.cpp
@@ -0,0 +1,25 @@
+//31)Next Permutation
+/*Given an array of integers nums, find the next permutation of nums.*/
+
+using namespce std;
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        // next_permutation(nums.begin(),nums.end());
+        int n = nums.size(), i;
+        for (i = n - 2; i >= 0; --i) {
+            if (nums[i] < nums[i + 1]) {
+                break;
+            }
+        }
+        if (i >= 0) {
+            for (int j = n - 1; j > i; --j) {
+                if (nums[j] > nums[i]) {
+                    swap(nums[i], nums[j]);
+                    break;
+                }
+            }
+        }
+        reverse(nums.begin() + i + 1, nums.end());
+    }
+};
