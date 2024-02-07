d40cbc90c12d5adbc0b3105b38f931b9637835ad
Sujal Gupta <111141823+Sujal2806@users.noreply.github.com>
2024-02-07 10:05:23 +0530

    Search Insert Position

---
+++ Q35.cpp
@@ -0,0 +1,22 @@
+//35)Search Insert Position
+/*Given a sorted array of distinct integers and a target value, return the index if the target is found. 
+If not, return the index where it would be if it were inserted in order.*/
+
+using namespace std;
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int l = 0, h = nums.size() - 1;
+        while (l <= h) {
+            int m = (l + h) / 2;
+            if (nums[m] == target) {
+                return m;
+            } else if (nums[m] < target) {
+                l = m + 1;
+            } else {
+                h = m - 1;
+            }
+        }
+        return l;
+    }
+};
