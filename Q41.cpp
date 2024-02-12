f24739c605d8bb2b39b784e7a3a1be3d12fc902b
Sujal Gupta <111141823+Sujal2806@users.noreply.github.com>
2024-02-12 17:50:02 +0530

    First Missing Positive

---
+++ Q41.cpp
@@ -0,0 +1,20 @@
+//41)First Missing Positive
+/*Given an unsorted integer array nums, return the smallest missing positive integer.*/
+
+using namespace std;
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        map<int, int> m;
+        for (int i = 0; i < n; i++) {
+            m[nums[i]]++;
+        }
+        for (int i = 1;; i++) {
+            if (m[i] == 0) {
+                return i;
+            }
+        }
+    }
+};
